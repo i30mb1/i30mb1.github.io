6222b6c687259f342e8a3643da99c9354d8da361
i30mb1 <fate.i30mb1@gmail.com>
2019-05-08 23:28:11 +0300

    1

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 
 var db = firebase.firestore();
 function storeData() {
-db.collection("clients").doc("+n7").set({name:"done"})
-.then(function() {
-    console.log("Document successfully written!");
-})
-.catch(function(error) {
-    console.error("Error writing document: ", error);
-});
+db.collection("clients").doc("+375259721657").set({});
 }
