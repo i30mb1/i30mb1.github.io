4e92c6267ed5de774a76d8854d5068ddac1878bc
i30mb1 <fate.i30mb1@gmail.com>
2019-05-08 23:25:55 +0300

    1

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-window.alert("OKA");
+
 var db = firebase.firestore();
 function storeData() {
-db.collection("clients").doc("+n7").set({name:"done"});
+db.collection("clients").doc("+n7").set({name:"done"})
+.then(function() {
+    console.log("Document successfully written!");
+})
+.catch(function(error) {
+    console.error("Error writing document: ", error);
+});
 }
